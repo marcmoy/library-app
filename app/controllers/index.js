5fa1c83c167b4be2b39a4a1597d07afb1b3c3998
Marc Moy <moymarc453@gmail.com>
2016-11-29 13:03:40 -0800

    first action

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,16 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisabled: true,
-
   emailAddress: '',
 
-  actualEmailAddress: Ember.computed('emailAddress', function() { 
-    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-  }),
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
 
-  emailAddressChanged: Ember.observer('emailAddress', function() { 
-    console.log('observer is called', this.get('emailAddress')); 
-  })
-
+  actions: {
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
 });
