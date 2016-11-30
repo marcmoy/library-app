523fc7cc22b58f3106e93c416fd77086cb0823e7
Marc Moy <moymarc453@gmail.com>
2016-11-30 00:17:10 -0800

    contact page

---
+++ app/controllers/contact.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+
+import Ember from 'ember';
+
+const CHARLIMIT = 1000;
+const FEEDBACK = {
+  normal: 'form-group has-feedback',
+  success: 'form-group has-feedback has-success'
+};
+
+export default Ember.Controller.extend({
+
+  // Email
+  email: '',
+  emailFeedback: FEEDBACK['normal'],
+  validEmail: Ember.computed.match('email', /^.+@.+\..+$/),
+  changeEmailFeedback: Ember.observer('email', function() {
+    let feedback = this.get('validEmail') ? 'success' : 'normal';
+    this.set('emailFeedback', FEEDBACK[feedback]);
+    this.set('responseMessage', '');
+  }),
+
+  // Message
+  msg: '',
+  msgFeedback: FEEDBACK['normal'],
+  validMsg: Ember.computed.gte('msg.length', 5),
+  changeMsgFeedback: Ember.observer('msg', function() {
+    let feedback = this.get('validMsg') ? 'success' : 'normal';
+    this.set('msgFeedback', FEEDBACK[feedback]);
+    this.set('responseMessage', '');
+  }),
+
+  // Character Limit
+  charLimit: String(CHARLIMIT),
+  charCount: String(CHARLIMIT),
+  updateCharLimit: Ember.observer('msg', function() {
+    let count = CHARLIMIT - this.get('msg').length;
+    this.set('charCount', String(count));
+  }),
+
+  // Send message
+  readyToSend: Ember.computed.and('validEmail', 'validMsg'),
+  responseMessage: '',
+  displayResponse: Ember.computed.gte('responseMessage.length', 1),
+  actions: {
+    sendMessage() {
+      alert('Sending your message...');
+      this.set('email', '');
+      this.set('msg', '');
+      this.set('responseMessage', 'Thank you! We will reply to you shortly.');
+    }
+  }
+});
